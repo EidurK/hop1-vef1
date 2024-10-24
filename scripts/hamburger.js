93dae039db6a5a974c405b825289e0457f284261
Eidur <ek040803@gmail.com>
2024-10-24 20:41:46 +0000

    Ætla að skila

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,8 +1,9 @@
+
 function ShowHide(x) {
   x.classList.toggle("change");
   var Menu = document.getElementById("Menu");
   if (Menu.style.display === "none") {
-    Menu.style.display = "flex";
+    Menu.style.display = "grid";
   } else {
     Menu.style.display = "none";
   }
